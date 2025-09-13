0feb0afdb0aa31a842849514ad13ca959711eabe
Marco Paoletta <marcopaoletta2007@gmail.com>
2025-09-13 09:43:50 -0300

    First attack animation

--- Assets/Scripts/PlayerEquipment.cs
+++ Assets/Scripts/PlayerEquipment.cs
@@ -4,8 +4,25 @@ public class PlayerEquipment : MonoBehaviour
 {
     [SerializeField] private GameObject sword;
 
+    private bool canAttack;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void TurnOnSword()
     {
         sword.SetActive(true);
+        canAttack = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canAttack)
+        {
+            animator.SetTrigger("SwordAttack");
+        }
     }
 }
