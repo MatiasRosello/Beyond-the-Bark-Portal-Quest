c3497a08de42222bbd5512365251d2461f0072ce
manumabe12 <manu.mabellini@gmail.com>
2025-09-21 12:29:47 -0300

    objetive text

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -6,11 +8,20 @@ public class EnemyManager : MonoBehaviour
     public Transform keySpawnPoint; 
     private int defeatedEnemies = 0;
     [SerializeField] private int enemiesToDefeat;
+    [SerializeField] private TextMeshProUGUI statusText;
 
     public void EnemyDefeated()
     {
+        
+        if (defeatedEnemies == 0)
+        {
+            statusText.gameObject.SetActive(true);
+        }
+
         defeatedEnemies++;
         Debug.Log("Enemigo derrotado. Total: " + defeatedEnemies);
+
+        UpdateUIText();
 
         if (defeatedEnemies >= enemiesToDefeat)
         {
@@ -32,6 +43,39 @@ public class EnemyManager : MonoBehaviour
             Debug.LogError("El prefab de la llave o el punto de aparición no están asignados.");
         }
     }
+
+    private void UpdateUIText()
+    {
+        if (statusText != null)
+        {
+            if (defeatedEnemies < enemiesToDefeat)
+            {
+                statusText.text = "Enemigos muertos " + defeatedEnemies + "/" + enemiesToDefeat;
+            }
+            else
+            {
+                statusText.text = "Todos los enemigos han muerto, recoge la llave para avanzar.";
+
+                StartCoroutine(HideTextAfterDelay(3f));
+            }
+        }
+    }
+
+    // Coroutine para ocultar el texto
+    private IEnumerator HideTextAfterDelay(float delay)
+    {
+        // Espera 'delay' segundos antes de continuar
+        yield return new WaitForSeconds(delay);
+
+        // Desactiva el objeto de texto, haciéndolo invisible
+        if (statusText != null)
+        {
+            statusText.gameObject.SetActive(false);
+        }
+    }
+
+
+
 }
 
 
