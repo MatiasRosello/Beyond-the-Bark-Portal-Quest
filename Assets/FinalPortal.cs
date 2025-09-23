0f1571a4ec40cd33d68076f0651ca031b538b2ac
manumabe12 <manu.mabellini@gmail.com>
2025-09-22 23:15:32 -0300

    Portallvl3

--- Assets/FinalPortal.cs
+++ Assets/FinalPortal.cs
@@ -16,35 +16,32 @@ public class FinalPortal : MonoBehaviour
 
         if (otherObject.CompareTag("Player"))
         {
+            // Busca todos los GameObjects con el tag "Enemy" en la escena
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-            ThirdPersonController thirdPersonController = otherObject.GetComponent<ThirdPersonController>();
-            if (thirdPersonController != null)
+            // Si no hay enemigos (la longitud del array es 0), procede con la lógica del portal
+            if (enemies.Length == 0)
             {
-                thirdPersonController.GetComponent<Animator>().SetFloat("Speed", 0);
-                thirdPersonController.enabled = false;
+                // La lógica de la escena anterior y el personaje se ejecutan solo si NO hay enemigos
+                ThirdPersonController thirdPersonController = otherObject.GetComponent<ThirdPersonController>();
+                if (thirdPersonController != null)
+                {
+                    thirdPersonController.GetComponent<Animator>().SetFloat("Speed", 0);
+                    thirdPersonController.enabled = false;
+                }
+
+                // Inicia la transición a la siguiente escena
+                SceneTransitionManager.Instance.FadeIn(
+                    null,
+                    () => SceneManager.LoadScene(nextSceneName),
+                    null);
             }
-
-
-            SceneTransitionManager.Instance.FadeIn(
-                null,
-                () => SceneManager.LoadScene(nextSceneName),
-                null);
+            else
+            {
+                // Si hay enemigos, el personaje NO se detiene y se muestra un mensaje de debug
+                Debug.Log("El boss está vivo, eliminalo para avanzar");
+            }
         }
-
-        // else if (otherObject.CompareTag("Enemy"))
-        // {
-
-        //     GameObject boss = GameObject.FindWithTag("Boss");
-
-
-        //     if (boss == null)
-        //     {
-
-        //         SceneTransitionManager.Instance.FadeIn(
-        //             null,
-        //             () => SceneManager.LoadScene(nextSceneName),
-        //             null);
-        //     }
-        // }
     }
 }
+
