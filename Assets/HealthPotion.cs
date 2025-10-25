a7a32a9ce9238c0b8d10f25010995440fc72bd36
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2025-10-25 14:38:22 -0300

    pocion de vida

--- Assets/HealthPotion.cs
+++ Assets/HealthPotion.cs
@@ -4,6 +4,23 @@ public class HealthPotion : MonoBehaviour
 {
 
     [SerializeField] private float lifeToHeal = 25f;
+
+    private void Start()
+    {
+        // Si tiene Rigidbody, usar MovePosition en lugar de transform.position
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Vector3 newPosition = new Vector3(transform.position.x, 0.25f, transform.position.z);
+            rb.MovePosition(newPosition);
+        }
+        else
+        {
+            // Si no tiene Rigidbody, usar el método normal
+            transform.position = new Vector3(transform.position.x, 0.25f, transform.position.z);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
