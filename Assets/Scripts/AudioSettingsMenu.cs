2e870a8a12bb973b84e7ca647107d964e710e6ce
ValenTheXD <v4l3nt1n888@gmail.com>
2025-10-27 23:45:27 -0300

    slider2
    no cambio casi nada, creo

---
+++ Assets/Scripts/AudioSettingsMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettingsMenu : MonoBehaviour
+{
+    [Header("Referencias")]
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    [Header("Parámetros del Mixer")]
+    public string musicParameter = "MusicVolume";
+    public string sfxParameter = "SFXVolume";
+
+    private void Start()
+    {
+        
+        float musicValue = PlayerPrefs.GetFloat(musicParameter, 0.75f);
+        float sfxValue = PlayerPrefs.GetFloat(sfxParameter, 0.75f);
+
+        musicSlider.value = musicValue;
+        sfxSlider.value = sfxValue;
+
+        SetMusicVolume(musicValue);
+        SetSFXVolume(sfxValue);
+
+       
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        
+        audioMixer.SetFloat(musicParameter, Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat(musicParameter, value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        audioMixer.SetFloat(sfxParameter, Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat(sfxParameter, value);
+    }
+}
